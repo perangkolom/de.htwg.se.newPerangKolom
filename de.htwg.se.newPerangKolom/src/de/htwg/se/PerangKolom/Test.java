65d1674a8f16ca59fe72b47ba24aa04aed7a7d2b
David7777 <dr93@hotmail.de>
2014-01-21 17:05:02 +0100

    test for branch conflict

---
+++ de.htwg.se.newPerangKolom/src/de/htwg/se/PerangKolom/Test.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package de.htwg.se.PerangKolom;
+
+/**
+ * @author David
+ *
+ */
+public class Test {
+	
+	public String test() {
+		String value = "Hallo Welt!";
+		
+		return value;
+	}
+
+}
