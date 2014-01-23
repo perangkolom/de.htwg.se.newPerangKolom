2d7e3a92deddcee75774eca67c932f174ffa105e
David7777 <dr93@hotmail.de>
2014-01-23 16:50:11 +0100

    delete all merge-conflicts, now it is working

--- de.htwg.se.newPerangKolom/src/de/htwg/se/PerangKolom/controller/IPerangKolomController.java
+++ de.htwg.se.newPerangKolom/src/de/htwg/se/PerangKolom/controller/IPerangKolomController.java
@@ -31,9 +31,8 @@ public interface IPerangKolomController extends IObservable {
 	String showHelp();
 	
 	String getShortInstructions();
-<<<<<<< HEAD
 	
-//David specifisch
+//David specific
 	
 	public void setCellValue(int row, int column, int value);
 	
@@ -46,20 +45,6 @@ public interface IPerangKolomController extends IObservable {
 	public boolean fourBordersFilled(int x, int y);
 	
 	public void setGridSize(int numberOfColumns, int numberOfRows);
-=======
 
-//David specific
-	
-	public void setCellValue(int row, int column, int value);
-	
-	public int getRandNumber(int x, int y);
-	
-	public void setBorderFilled(int x, int y, int borderType);
-	
-	public Cell getCell(int x, int y);
-	
-	
->>>>>>> refs/heads/newModel1
-	
 	
 }
