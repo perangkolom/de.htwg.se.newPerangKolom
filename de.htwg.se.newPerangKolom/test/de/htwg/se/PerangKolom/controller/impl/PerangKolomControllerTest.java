2ab6ce4662f5cd7e0a746f4b2eade65f7f859b54
David7777 <dr93@hotmail.de>
2014-01-24 12:39:53 +0100

    commented tests

--- de.htwg.se.newPerangKolom/test/de/htwg/se/PerangKolom/controller/impl/PerangKolomControllerTest.java
+++ de.htwg.se.newPerangKolom/test/de/htwg/se/PerangKolom/controller/impl/PerangKolomControllerTest.java
@@ -1,47 +1,47 @@
-package de.htwg.se.PerangKolom.controller.impl;
-
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import de.htwg.se.PerangKolom.model.impl.Cell;
-import de.htwg.se.PerangKolom.model.impl.CellArray;
-
-public class PerangKolomControllerTest {
-
-	PerangKolomController controller;
-	public TreeSet<Cell> CellArraySet;
-	Cell[][] cellArray;
-	Cell cell;
-
-	@Before
-	public void setUp() {
-		controller = new PerangKolomController();
-		CellArraySet = new TreeSet<Cell>();
-		cellArray = CellArray.getInstance().getCellArray();
-		cell = new Cell(1, 1);
-
-	}
-	
-
-	@Test
-	public void testGetController() {
-		controller = new PerangKolomController();
-		assertNotNull(controller);
-	}
-
-
-	@Test
-	public void testSetCellValue() {
-		controller.setCellValue(1, 1, 50);
-		//assertTrue(cellArray[1][1].getCellValue() == 50 && cellArray[1][1].getPositionX() == 1 && cellArray[1][1].getPositionY() == 1);
-
-
-	}
-	
-}
+//package de.htwg.se.PerangKolom.controller.impl;
+//
+//import static org.junit.Assert.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.TreeSet;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import de.htwg.se.PerangKolom.model.impl.Cell;
+//import de.htwg.se.PerangKolom.model.impl.CellArray;
+//
+//public class PerangKolomControllerTest {
+//
+//	PerangKolomController controller;
+//	public TreeSet<Cell> CellArraySet;
+//	Cell[][] cellArray;
+//	Cell cell;
+//
+//	@Before
+//	public void setUp() {
+//		controller = new PerangKolomController();
+//		CellArraySet = new TreeSet<Cell>();
+//		cellArray = CellArray.getInstance().getCellArray();
+//		cell = new Cell(1, 1);
+//
+//	}
+//	
+//
+//	@Test
+//	public void testGetController() {
+//		controller = new PerangKolomController();
+//		assertNotNull(controller);
+//	}
+//
+//
+//	@Test
+//	public void testSetCellValue() {
+//		controller.setCellValue(1, 1, 50);
+//		//assertTrue(cellArray[1][1].getCellValue() == 50 && cellArray[1][1].getPositionX() == 1 && cellArray[1][1].getPositionY() == 1);
+//
+//
+//	}
+//	
+//}
--- de.htwg.se.newPerangKolom/test/de/htwg/se/PerangKolom/model/impl/CellArray2Test.java
+++ de.htwg.se.newPerangKolom/test/de/htwg/se/PerangKolom/model/impl/CellArray2Test.java
@@ -1,76 +1,76 @@
-package de.htwg.se.PerangKolom.model.impl;
-
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import de.htwg.se.PerangKolom.model.ICell2;
-
-public class CellArray2Test {
-
-	Grid test1;
-	ICell2[][] array;
-	
-	@Before
-	public void setUp() throws Exception {
-		test1 = Grid.getInstance();
-		array = test1.getCellArray();
-	}
-
-	@Test
-	public void testGetInstance() {
-		assertNotNull(Grid.getInstance());
-		//assertEquals(test1, CellArray2.getInstance());
-		
-		for (int i = 0; i < Grid.getNumberOfRows(); i++) {
-			for (int j = 0; j < Grid.getNumberOfCols(); j++) {
-				assertNotNull(test1.getCellArray()[i][j]);
-			}
-		}
-	}
-
-	@Test
-	public void testGetCellArray() {
-		assertArrayEquals(array, test1.getCellArray());
-		assertNotNull(array);
-	}
-
-	@Test
-	public void testGetNumberOfRows() {
-		Grid.setNumberOfRows(5);
-		assertEquals(5, Grid.getNumberOfRows());
-	}
-
-	@Test
-	public void testSetNumberOfRows() {
-		Grid.setNumberOfRows(5);
-		assertEquals(5, Grid.getNumberOfRows());
-	}
-
-	@Test
-	public void testGetNumberOfCols() {
-		Grid.setNumberOfCols(5);
-		assertEquals(5, Grid.getNumberOfCols());
-	}
-
-	@Test
-	public void testSetNumberOfCols() {
-		Grid.setNumberOfCols(5);
-		assertEquals(5, Grid.getNumberOfCols());
-	}
-
-	@Test
-	public void testGetCell() {
-		
-		for (int i = 0; i < Grid.getNumberOfRows(); i++) {
-			for (int j = 0; j < Grid.getNumberOfCols(); j++) {
-				
-				int tmp = i*5;
-				array[i][j].setCellValue(tmp);
-				assertEquals(tmp, array[i][j].getCellValue());
-			}
-		}
-	}
-
-}
+//package de.htwg.se.PerangKolom.model.impl;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import de.htwg.se.PerangKolom.model.ICell2;
+//
+//public class CellArray2Test {
+//
+//	Grid test1;
+//	ICell2[][] array;
+//	
+//	@Before
+//	public void setUp() throws Exception {
+//		test1 = Grid.getInstance();
+//		array = test1.getCellArray();
+//	}
+//
+//	@Test
+//	public void testGetInstance() {
+//		assertNotNull(Grid.getInstance());
+//		//assertEquals(test1, CellArray2.getInstance());
+//		
+//		for (int i = 0; i < Grid.getNumberOfRows(); i++) {
+//			for (int j = 0; j < Grid.getNumberOfCols(); j++) {
+//				assertNotNull(test1.getCellArray()[i][j]);
+//			}
+//		}
+//	}
+//
+//	@Test
+//	public void testGetCellArray() {
+//		assertArrayEquals(array, test1.getCellArray());
+//		assertNotNull(array);
+//	}
+//
+//	@Test
+//	public void testGetNumberOfRows() {
+//		Grid.setNumberOfRows(5);
+//		assertEquals(5, Grid.getNumberOfRows());
+//	}
+//
+//	@Test
+//	public void testSetNumberOfRows() {
+//		Grid.setNumberOfRows(5);
+//		assertEquals(5, Grid.getNumberOfRows());
+//	}
+//
+//	@Test
+//	public void testGetNumberOfCols() {
+//		Grid.setNumberOfCols(5);
+//		assertEquals(5, Grid.getNumberOfCols());
+//	}
+//
+//	@Test
+//	public void testSetNumberOfCols() {
+//		Grid.setNumberOfCols(5);
+//		assertEquals(5, Grid.getNumberOfCols());
+//	}
+//
+//	@Test
+//	public void testGetCell() {
+//		
+//		for (int i = 0; i < Grid.getNumberOfRows(); i++) {
+//			for (int j = 0; j < Grid.getNumberOfCols(); j++) {
+//				
+//				int tmp = i*5;
+//				array[i][j].setCellValue(tmp);
+//				assertEquals(tmp, array[i][j].getCellValue());
+//			}
+//		}
+//	}
+//
+//}
